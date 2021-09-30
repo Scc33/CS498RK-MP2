c390a3f2cc426faceaabf3d5bd155203179ca321
Sean Coughlin <seanmc4@illinois.edu>
2021-09-29 19:15:56 -0500

    Add notes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+//https://codepen.io/gaearon/pen/LzWZvb?editors=0010
+//https://gitlab.com/uiuc-web-programming/react-demo
+//https://uiuc-web-programming.gitlab.io/fa21/slides/routing_and_state_11.pdf (routing/state)
+//https://gitlab.com/uiuc-web-programming/mp2
+//https://developer.marvel.com/account
+
 function App() {
   return (
     <div className="App">
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+/*const userAction = async () => {
+  const response = await fetch('http://example.com/movies.json');
+  const myJson = await response.json(); //extract JSON from the http response
+  // do something with myJson
+}*/
