c320be20b85b74e2af79f2f6e6c4db204e86a161
Sean Coughlin <seanmc4@illinois.edu>
2021-10-11 11:22:07 -0500

    Filter by movie score

--- src/components/ListView/index.js
+++ src/components/ListView/index.js
@@ -1,15 +1,16 @@
 import { Component } from 'react';
 import './styles.scss';
-import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import FormControl from '@mui/material/FormControl';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import axios from 'axios';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/fontawesome-free-solid'
 
@@ -19,7 +20,9 @@ class ListView extends Component {
         popularMovies: [],
         popularTVs: [],
         type: "movie",
-        order: "ascending"
+        order: "ascending",
+        rating: "0",
+        titleSearch: ""
     }
 
     constructor(props) {
@@ -27,6 +30,7 @@ class ListView extends Component {
         this.handleTypeChange = this.handleTypeChange.bind(this);
         this.handleSearchChange = this.handleSearchChange.bind(this);
         this.handleOrderChange = this.handleOrderChange.bind(this);
+        this.handleRatingChange = this.handleRatingChange.bind(this);
     }
 
     clear = () => {
@@ -39,15 +43,18 @@ class ListView extends Component {
         this.setState({ titleSearch: value });
         var filter;
         if (this.state.type === "movie") {
+            console.log(this.state.rating);
             filter = this.state.popularMovies.filter(content => (
-                content.title.toLowerCase().includes(value.toLowerCase())
+                content.title.toLowerCase().includes(value.toLowerCase()) &&
+                content.vote_average >= parseFloat(this.state.rating)
             ));
             if (this.state.order === "descending") {
                 filter.reverse();
             }
         } else {
             filter = this.state.popularTVs.filter(content => (
-                content.name.toLowerCase().includes(value.toLowerCase())
+                content.name.toLowerCase().includes(value.toLowerCase()) &&
+                content.vote_average >= parseFloat(this.state.rating)
             ));
             if (this.state.order === "descending") {
                 filter.reverse();
@@ -88,6 +95,29 @@ class ListView extends Component {
             [key]: value
         });
     };
+
+    handleRatingChange(event){
+        const { value } = event.target;
+        this.setState({ rating: value });
+        var filter;
+        if (this.state.type === "movie") {
+            console.log(this.state.rating);
+            filter = this.state.popularMovies.filter(content => (
+                content.vote_average >= parseFloat(value)
+            ));
+            if (this.state.order === "descending") {
+                filter.reverse();
+            }
+        } else {
+            filter = this.state.popularTVs.filter(content => (
+                content.vote_average >= parseFloat(value)
+            ));
+            if (this.state.order === "descending") {
+                filter.reverse();
+            }
+        }
+        this.setState({ filter });
+    }
 
     //API: f052c50e624989f8ef4a5acc45dfc7f2
     componentDidMount() {
@@ -144,6 +174,20 @@ class ListView extends Component {
                                         <FormControlLabel value="descending" control={<Radio />} label="Descending" />
                                     </div>
                                 </RadioGroup>
+                                <FormControl sx={{ m: 1, minWidth: 120 }}>
+                                    <InputLabel id="demo-simple-select-label">Rating</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        label="rating"
+                                        defaultValue="0"
+                                        onChange={this.handleRatingChange}
+                                    >
+                                        <MenuItem value="0">All Movies</MenuItem>
+                                        <MenuItem value="9">Greater than 9</MenuItem>
+                                        <MenuItem value="8">Greater than 8</MenuItem>
+                                        <MenuItem value="7">Greater than 7</MenuItem>
+                                    </Select>
+                                </FormControl>
                             </div>
                         </FormControl>
                     </CardContent>
